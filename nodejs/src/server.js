e15c190e59a4c140b533327b8be2d42ed1e02dc1
votanloccntt <votanloccntt@gmail.com>
2022-11-14 12:57:48 +0700

    hoan thanh database

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 let app = express();
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRouters(app);
+
+connectDB();
 
 let port = process.env.PORT || 6969; //port === undefined => port =6969
 app.listen(port, () => {
