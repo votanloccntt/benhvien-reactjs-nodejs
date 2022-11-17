02cb025ed4b0f1a337b1e82ac8de596cd7645444
votanloccntt <votanloccntt@gmail.com>
2022-11-17 22:28:20 +0700

    create a new user

--- nodejs/src/controllers/homeController.js
+++ nodejs/src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
@@ -12,7 +13,17 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
--- nodejs/src/route/web.js
+++ nodejs/src/route/web.js
@@ -1,10 +1,14 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+
 let router = express.Router();
+
 let initWebRouters = (app) => {
   //rest api
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
   return app.use("/", router);
 };
 module.exports = initWebRouters;
